bd72c5003b136b3029f35a96f2f8836baa3b1d73
stevenkreutzer <steven.kreutzer@yahoo.com>
2022-02-09 23:47:10 -0500

    initial commit

--- src/main/java/com/example/project/LList.java
+++ src/main/java/com/example/project/LList.java
@@ -3,102 +3,108 @@ import java.util.Scanner;
 public class LList<T>
 {
 	Node head;
-	
+
 	private class Node
 	{
 		T item;
 		Node next;
-		
+
 		public Node(T item, Node next)
 		{
 			this.item = item;
 			this.next = next;
 		}
 	}
-	
-	public  LList()
+
+	public LList()
 	{
-		
+
 	}
-	
+
 	public LList(LList other)
 	{
-		
+
 	}
-	
+
 	public LList(T[] other)
 	{
-		
+		for (int i=other.length-1; i>=0; i--)
+			add(other[i]);
 	}
-	
+
 	public void add(T item)
 	{
 		head = new Node(item, head);
 	}
-	
+
 	public void addToEnd(T item)
 	{
-		
+
 	}
-	
+
 	public T removeFromFront()
 	{
 		if (head == null)
 			return null;
-		
+
 		T result = head.item;
 		head = head.next;
-		
+
 		return result;
 	}
-	
+
 	public int getIndex()
 	{
 		return -100;
 	}
-	
+
 	private Node getPredecessor(T item)
 	{
 		Node previousNode = null;
 		for (Node ptr = head; ptr != null; ptr = ptr.next)
 		{
 			if (ptr.item.equals(item))
-				return previousNode;	
+				return previousNode;
 			else
 				previousNode = ptr;
 		}
 
 		return null;
 	}
-	
+
 	public int size()
 	{
 		return -1;
 	}
-	
+
 	public int count(T item)
 	{
-		return -1;
+		int count = 0;
+
+		for (Node ptr = head; ptr != null; ptr = ptr.next)
+			if (ptr.item.equals(item))
+				++count;
+		return count;
 	}
-	
+
 	public T delete(T item)
 	{
 		if (head == null)
 			return null;
-		Node previousNode = getPredecessor(item);		
+		Node previousNode = getPredecessor(item);
 		if (previousNode == null)
 			if (head.item.equals(item))
 				return removeFromFront();
-			else 
-				return null;		
-		
+			else
+				return null;
+
 		previousNode.next = previousNode.next.next;
 		return null;
 	}
 
 	public void insertAt(int index, T item)
 	{
-		
+
 	}
 
 	public T[] toArray()
@@ -106,15 +112,15 @@ public class LList<T>
 		int count = 0;
 		for (Node ptr = head; ptr != null; ptr = ptr.next)
 			++count;
-			
+
 		T[] array = (T[]) new Object[count];
 		int index = 0;
-		
+
 		for (Node ptr = head; ptr != null; ptr = ptr.next)
-			array[index++]  = ptr.item;
+			array[index++] = ptr.item;
 		return array;
 	}
-	
+
 	public boolean replace(T oldItem, T newItem)
 	{
 		return false;
@@ -124,7 +130,7 @@ public class LList<T>
 	{
 		return false;
 	}
-	
+
 	public LList reverse()
 	{
 		return null;
@@ -133,26 +139,26 @@ public class LList<T>
 	public String toString()
 	{
 		String result = "";
-		
-		for (Node ptr=head; ptr!=null; ptr=ptr.next)
+
+		for (Node ptr = head; ptr != null; ptr = ptr.next)
 			result += ptr.item + " ";
-		
+
 		return result;
 	}
 
 	public static void main(String[] args)
 	{
 		Scanner in = new Scanner(System.in);
-		
-		LList<Integer>  lList = new LList<>(); 
-		
+
+		LList<Integer> lList = new LList<>();
+
 		while (true)
 		{
 			System.out.println("1. add");
 			System.out.println("2. remove from front");
 			System.out.println("9. toString");
-			
-			switch(in.nextInt())
+
+			switch (in.nextInt())
 			{
 			case 1:
 				System.out.println("Enter item");

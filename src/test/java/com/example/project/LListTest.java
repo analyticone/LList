53765ca09bc7ed67317b201075c3628ca934ade8
stevenkreutzer <steven.kreutzer@yahoo.com>
2022-02-09 23:22:51 -0500

    initial file

---
+++ src/test/java/com/example/project/LListTest.java
@@ -0,0 +1,98 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LListTest
+{
+
+	@Test
+	void testLListLList()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testLListTArray()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAdd()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAddToEnd()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testRemoveFromFront()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetIndex()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testSize()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testCount()
+	{
+		assertEquals(2, new LList<Integer>(new Integer[] {1,0,0,1}).count(1));
+	}
+
+	@Test
+	void testDelete()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testInsertAt()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testToArray()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testReplace()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testReplaceAll()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testReverse()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testToString()
+	{
+		fail("Not yet implemented"); // TODO
+	}
+
+}
